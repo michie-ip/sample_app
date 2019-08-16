f54778e54433fa93e2ce820cedf18f1a29220362
Michelle Ip <mip@jiffyondemand.com>
2019-08-16 17:08:03 -0400

    add a full_title helper and a not in use example_user for reference

---
+++ example_user.rb
@@ -0,0 +1,22 @@
+class User
+  attr_accessor :first, :last, :email
+
+  def init(attributes = {})
+    @first = attributes[:first]
+    @last = attributes[:last]
+    @email = attributes[:email]
+    @alpha =[@first, @last].sort
+  end
+
+  def full_name
+    @full = "#{first} #{last}"
+  end
+
+  def formatted_email
+    "#{@full} <#{@email}>"
+  end
+
+  def alphabetical_name
+    "#{@last}, #{@first}"
+  end
+end

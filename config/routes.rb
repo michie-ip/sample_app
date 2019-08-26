6b543d99e5f4ac114dd18a082307ee9178231510
Michelle Ip <mip@jiffyondemand.com>
2019-08-26 13:44:23 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'static_pages#home'
 
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  
-  get '/signup', to: 'users#new'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
-  
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
+
   # Notes on as:
   # get '/help', to: 'static_pages#help', as: 'helf'
 end
